90af853b86d45cbd63ae8fa72400b6f0b25d1c29
fareez-lic <fareez.lic@gmail.com>
2026-08-24 03:07:15 -0400

    Add ssh_allowed_cidr variable for SSH access terraform: add restricted SSH CIDR variable
    Add variable for SSH allowed CIDR with description.

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -4,3 +4,8 @@ variable "instance_type" {
 variable "key_name" {
   default = "devops-node-key"
 }
+
+variable "ssh_allowed_cidr" {
+  description = "CIDR block allowed to connect over SSH, for example 203.0.113.10/32"
+  type        = string
+}
